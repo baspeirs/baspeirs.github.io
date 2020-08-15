f809aebe69835cc069de1c3e06e0730d24eae2d0
Bryce Speirs <baspeirs@gmail.com>
2020-08-15 00:50:31 -0400

    Updates

---
+++ precache-manifest.c9fb7d8276bbb6874b20731cc1bb3993.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7419b1febf5cf2fe432b9c7039ebdd90",
+    "url": "/baspeirs.github.io/index.html"
+  },
+  {
+    "revision": "859b05311b0926647392",
+    "url": "/baspeirs.github.io/static/css/main.649357c9.chunk.css"
+  },
+  {
+    "revision": "501db0cdbf27a79452de",
+    "url": "/baspeirs.github.io/static/js/2.e80b64d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/baspeirs.github.io/static/js/2.e80b64d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "859b05311b0926647392",
+    "url": "/baspeirs.github.io/static/js/main.d0983407.chunk.js"
+  },
+  {
+    "revision": "fecba3dfbe3c7f484d4d",
+    "url": "/baspeirs.github.io/static/js/runtime-main.c21b0e5c.js"
+  },
+  {
+    "revision": "1d801dd5a6a5e44b7503e433b3a540b5",
+    "url": "/baspeirs.github.io/static/media/nasa-Q1p7bh3SHj8-unsplash.1d801dd5.jpg"
+  }
+]);
