d0e8a0cd71b3bcca829f164fcde93aeecc869ac0
Bryce Speirs <baspeirs@gmail.com>
2021-04-06 19:28:15 -0400

    restyled footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,26 +1,37 @@
 import React from "react";
-import GitHubLogo from "../../assets/photos/GitHubLogo.svg";
-import LinkedInLogo from "../../assets/photos/LinkedInLogo.svg";
-import EmailLogo from "../../assets/photos/EmailLogo.svg";
+import GitHubLogo from "../../assets/photos/github.svg";
+import LinkedInLogo from "../../assets/photos/linkedin.svg";
+import EmailLogo from "../../assets/photos/email.svg";
 import "./style.css";
 
 function Footer() {
     return (
         <footer>
             <div className="container">
-                <div className="row">
-                    <p className="col-lg-2" id="signature">2020 Bryce Speirs</p>
-                    <div className="col-xl-8">
-                        <div className="row">
-                            <p className="col-xl-5" id="social-media">Social Media Links:</p>
-                            <div className="col-xl-5" id="contact-links">
-                                <div className="contact-image"><a href="https://github.com/baspeirs"><img src={GitHubLogo} alt="GitHub Logo" id="github-logo" /></a></div>
-                                <div className="contact-image"><a href="https://www.linkedin.com/in/bryce-speirs-bb1319199/"><img src={LinkedInLogo} alt="GitHub Logo" id="linkedin-logo" /></a></div>
-                                <div className="contact-image"><a href="mailto:baspeirs@gmail.com"><img src={EmailLogo} alt="GitHub Logo" id="mail-logo" /></a></div>
+                <div className="row" id="footer-content">
+                    <div className="col-lg-4" id="signature">
+                        <p >2020</p>
+                        <p>Bryce Speirs</p>
+                    </div>
+                    <div className="col-xl-4">
+                        <div className="row" id="contact-links">
+                            <div className="col-xl-4">
+                            <div className="contact-image"><a href="https://github.com/baspeirs"><img src={GitHubLogo} alt="GitHub Logo from Pixel Perfect" id="github-logo" /></a></div>
+
+                            </div>
+                            <div className="col-xl-4">
+                            <div className="contact-image"><a href="https://www.linkedin.com/in/bryce-speirs-bb1319199/"><img src={LinkedInLogo} alt="LinkedIn Logo from Freepik" id="linkedin-logo" /></a></div>
+                                
+                            </div>
+                            <div className="col-xl-4">
+                            <div className="contact-image"><a href="mailto:baspeirs@gmail.com"><img src={EmailLogo} alt="Email Logo from Freepik" id="mail-logo" /></a></div>
+                                
                             </div>
                         </div>
+                        {/* <div id="contact-links">
+                        </div> */}
                     </div>
-                    <div className="col-lg-2">
+                    <div className="col-lg-4">
                         <p >Contact Number:</p>
                         <a href="tel:5159549221">515-954-9221</a>
                     </div>
