c3a7d135e24df9a229a83261c16e9000d51b32cd
Bryce Speirs <baspeirs@gmail.com>
2022-03-09 09:02:56 -0500

    updated about section

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -3,7 +3,6 @@ import Container from "../../Container";
 import Row from "../../Row";
 import Column from "../../Column";
 import "./style.css";
-import BryceSpeirs from "../../../assets/photos/BryceSpeirs.jpg";
 function About() {
     return (
         <div id="about">
@@ -15,18 +14,19 @@ function About() {
                     </Column>
                 </Row>
                 <div id="top-content">
-                <Row>
+                {/* <Row>
                         <Column size="lg-12">
                             <div id="image-holder">
                                 <img id="profile-img" src={BryceSpeirs} alt="Bryce Speirs" />
                             </div>
                         </Column>
-                </Row>
+                </Row> */}
                 <Row>
                 <Column size="lg-12" id="about-description">
                             <div id="about-description">
+                            
+                            <p>I am a dedicated software engineer with experience in Full Stack Web Development and Robotic Process Automation. I truly enjoy my work as an engineer and am not afraid to branch out of my comfort zone. Outside of work I enjoy music, video games, and baseball.</p>
                             <p id="welcome">Thank you for viewing my portfolio!</p>
-                            <p>You may have noticed by now that I am a passionate and dedicated software engineer, with expeirence in full stack web development and Robotic Process Automation. I truly enjoy about my work as an engineer and am not afraid to branch out of my comfort zone. Outside of work, you can find me playing or listening to  music, enjoying any type of fantasy, playing video games, or watching baseball.</p>
                             </div>
                         </Column>
                 </Row>
